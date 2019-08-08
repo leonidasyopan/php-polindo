ef3692a5a21bddbf0d29890dd02fcde0ac6c1fed
Leonidas Yopan <leonidasyopan@gmail.com>
2019-08-08 11:55:52 -0300

    Created the home.php file

--- home.php
+++ home.php
@@ -30,6 +30,32 @@
         <p>Hello <?php Print "$user"?>!</p> <!-- Displays user's name -->
         <a href="logout.php">Click here to logout</a><br>
         
+        <form action="add.php" method="POST" id="add-item-form">
+            <fieldset>
+                <div class="form-item">
+                    <label for="details">Add more to list:</label>
+                    <input id="details" type="text" name="details">
+                </div>
+                <div class="form-item">
+                    <label for="public">public post? </label>
+                    <input type="checkbox" name="public" id="public" value="yes">
+                </div>
+                <div class="form-example">
+                    <input type="submit" value="Add to list">
+                </div>                
+            </fieldset>
+        </form>
+
+        <h2>My list</h2>
+        <table border="1px" width="100%">
+            <tr>
+                <th>Id</th>
+                <th>Details</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+        </table>
+
     </main>
     <footer>
     </footer>    
