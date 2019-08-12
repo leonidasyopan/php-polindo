548c5dbd0740d05683c34c39c80d7c690469c382
Leonidas Yopan <leonidasyopan@gmail.com>
2019-08-12 18:37:42 -0300

    Started the CSS layout for our dashboard

--- index.php
+++ index.php
@@ -10,49 +10,54 @@
     <link rel="stylesheet" href="css/styles.css" type="text/css">
 </head>
 <body>
-    <header>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="register.php">Register</a></li>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <h2>Lists</h2>
-        <table width="100%" border="1px">
-            <tr>
-                <th>Id</th>
-                <th>Details</th>
-                <th>Post Time</th>
-                <th>Edit Time</th>
-            </tr>
-            <?php
-                // Create Connection
-                $con = mysqli_connect("localhost","polindo","LIktRn4dpIvkfygY","php-polindo");
+    <div id="main-container">
+        <header class="header">
+            
+        </header>
+        <aside class="sidenav">
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="login.php">Login</a></li>
+                    <li><a href="register.php">Register</a></li>
+                </ul>
+            </nav>
+        </aside>
+        <main class="main">
+            <h2>Lists</h2>
+            <table width="100%" border="1px">
+                <tr>
+                    <th>Id</th>
+                    <th>Details</th>
+                    <th>Post Time</th>
+                    <th>Edit Time</th>
+                </tr>
+                <?php
+                    // Create Connection
+                    $con = mysqli_connect("localhost","polindo","LIktRn4dpIvkfygY","php-polindo");
 
-                // Check connection
-                if (mysqli_connect_errno()) {
-                echo "Failed to connect to MySQL: " . mysqli_connect_error();
-                }
+                    // Check connection
+                    if (mysqli_connect_errno()) {
+                    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+                    }
 
-                $query = mysqli_query($con, "SELECT * FROM list WHERE public='yes'"); // SQL Query for items with public being set to yes
+                    $query = mysqli_query($con, "SELECT * FROM list WHERE public='yes'"); // SQL Query for items with public being set to yes
 
-                while($row = mysqli_fetch_array($query))
-                {
-                    Print "<tr>";
-                        Print '<td align="center">' . $row['id'] . '</td>';
-                        Print '<td align="center">' . $row['details'] . '</td>';
-                        Print '<td align="center">' . $row['date_posted'] . ' - ' .  $row['time_posted'] . '</td>';
-                        Print '<td align="center">' . $row['date_edited'] . ' - ' .  $row['time_edited'] . '</td>';
-                    Print "</tr>";
-                }
-            ?>
+                    while($row = mysqli_fetch_array($query))
+                    {
+                        Print "<tr>";
+                            Print '<td align="center">' . $row['id'] . '</td>';
+                            Print '<td align="center">' . $row['details'] . '</td>';
+                            Print '<td align="center">' . $row['date_posted'] . ' - ' .  $row['time_posted'] . '</td>';
+                            Print '<td align="center">' . $row['date_edited'] . ' - ' .  $row['time_edited'] . '</td>';
+                        Print "</tr>";
+                    }
+                ?>
 
-        </table>
-    </main>
-    <footer>
-    </footer>    
+            </table>
+        </main>
+        <footer class="footer">
+        </footer>
+    </div>    
 </body>
 </html>
