1d667ff934f7da78eb08a7692ee1824605099296
Leonidas Yopan <leonidasyopan@gmail.com>
2019-08-13 17:00:43 -0300

    Minor changes to the CSS file

--- index.php
+++ index.php
@@ -58,8 +58,8 @@
             </table>
         </main>
         <footer class="footer">
-            <div class="footer__copyright">&copy; 2018 MTH</div>
-            <div class="footer__signature">Made with love by pure genius</div>
+            <div class="footer__copyright">&copy; 2019</div>
+            <div class="footer__signature">Only for testing purposes</div>
         </footer>
     </div>    
 </body>
